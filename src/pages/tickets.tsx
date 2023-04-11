c3d2233468c664f6b62b5eff3d86f4f94e6e9db1
Bostan Victor <victorbostan01@gmail.com>
2023-04-11 19:28:45 +0300

    Created tickets page, linked search button

--- src/pages/tickets.tsx
+++ src/pages/tickets.tsx
@@ -3,8 +3,19 @@
 // related to margins, check previous pages/components
 // TODO: use react query to fetch flights (getFlights), if the ticketMode from AppContext is set to flights, else - getTickets. print the results to the console.
 
+import { Layout, TicketForm } from "@/components";
+
 const Tickets = () => {
-  return <div>Tickets</div>;
+  return (
+    <Layout title="Tickets" hideFooter>
+      <div className="container mx-auto justify-between m-10">
+        <h1 className="text-4xl font-bold mb-6">Tickets</h1>
+        <div className="sticky top-0 z-40 p-6 pb-2">
+          <TicketForm />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Tickets;
