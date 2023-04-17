e9b11354bb13461daf0111ff65fe4f83e7e348bc
Daniela Vornic <daniela.vornic@gmail.com>
2023-04-17 18:49:55 +0300

    Update SwapButton.tsx

--- src/components/SwapButton.tsx
+++ src/components/SwapButton.tsx
@@ -5,7 +5,10 @@ export const SwapButton = () => {
   const { swapLocations } = useAppContext();
 
   return (
-    <button className="btn btn-outline bg-white mx-2" onClick={() => swapLocations()}>
+    <button
+      className="btn btn-outline bg-white mx-2 border-gray-300 hover:bg-gray-300 hover:text-gray-600 hover:border-gray-300"
+      onClick={() => swapLocations()}
+    >
       <VscArrowSwap className="text-md" />
     </button>
   );
